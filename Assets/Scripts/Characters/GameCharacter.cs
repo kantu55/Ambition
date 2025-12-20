bc2056ffa447e858e1a18997964fb3cfb1f18c1f
kantu55 <49814798+kantu55@users.noreply.github.com>
2025-12-20 18:11:49 +0900

    背景と立ち絵挿入

---
+++ Assets/Scripts/Characters/GameCharacter.cs
@@ -0,0 +1,9 @@
+﻿using UnityEngine;
+namespace Ambition.Characters
+{
+    public class GameCharacter : MonoBehaviour
+    {
+        [Header("コンポーネント")]
+        [SerializeField] private SpriteRenderer spriteRenderer;
+    }
+}
