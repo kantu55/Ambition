ab4a713d79f7e79dff95337cdb060b8ba65686f0
kantu55 <49814798+kantu55@users.noreply.github.com>
2026-01-03 16:56:43 +0900

    食事に関するデータを作成

---
+++ Assets/Scripts/DataStructures/FoodMitModel.cs
@@ -0,0 +1,45 @@
+﻿using Ambition.Utility;
+using System;
+
+namespace Ambition.DataStructures
+{
+    /// <summary>
+    /// 食事ステータス定義
+    /// </summary>
+    [Serializable]
+    public class FoodMitModel : IDataModel
+    {
+        private int id;
+        private string tier;
+
+        private string menuId;
+
+        private string menuName;
+        private string menuType;
+        private int mitigHP;
+        private int mitigMP;
+        private int mitigCOND;
+        private string notes;
+        public int Id => id;
+        public string Tier => tier;
+        public string MenuId => menuId;
+        public string MenuName => menuName;
+        public string MenuType => menuType;
+        public int MitigHP => mitigHP;
+        public int MitigMP => mitigMP;
+        public int MitigCOND => mitigCOND;
+        public string Notes => notes;
+
+        public void Initialize(CsvData data, int rowIndex)
+        {
+            tier = data.GetValue(rowIndex, "tier");
+            menuId = data.GetValue(rowIndex, "menu_id");
+            menuName = data.GetValue(rowIndex, "menu_name");
+            menuType = data.GetValue(rowIndex, "menu_type");
+            mitigHP = CsvHelper.ConvertToInt(data.GetValue(rowIndex, "mitig_HP"));
+            mitigMP = CsvHelper.ConvertToInt(data.GetValue(rowIndex, "mitig_MP"));
+            mitigCOND = CsvHelper.ConvertToInt(data.GetValue(rowIndex, "mitig_COND"));
+            notes = data.GetValue(rowIndex, "notes");
+        }
+    }
+}
---
+++ Assets/Scripts/DataStructures/FoodModel.cs
@@ -0,0 +1,46 @@
+﻿using Ambition.Utility;
+using System;
+
+namespace Ambition.DataStructures
+{
+    /// <summary>
+    /// 食事ステータス定義
+    /// </summary>
+    [Serializable]
+    public class FoodModel : IDataModel
+    {
+        // --- メンバ変数 ---
+        private int kind;
+        private int id;
+        private string name;
+        private int primary;
+        private int type;
+        private int monthlyCost;
+        private int costLimit;
+        private int mitigTotal;
+        private string notes;
+
+        // --- プロパティ ---
+        public int Kind => kind;
+        public int Id => id;
+        public string Name => name;
+        public int Primary => primary;
+        public int Type => type;
+        public int MonthlyCost => monthlyCost;
+        public int CostLimit => costLimit;
+        public int MitigTotal => mitigTotal;
+        public string Notes => notes;
+        public void Initialize(CsvData data, int rowIndex)
+        {
+            kind = CsvHelper.ConvertToInt(data.GetValue(rowIndex, "kind"));
+            id = CsvHelper.ConvertToInt(data.GetValue(rowIndex, "id"));
+            name = data.GetValue(rowIndex, "name");
+            primary = CsvHelper.ConvertToInt(data.GetValue(rowIndex, "primary"));
+            type = CsvHelper.ConvertToInt(data.GetValue(rowIndex, "type"));
+            monthlyCost = CsvHelper.ConvertToInt(data.GetValue(rowIndex, "monthly_cost"));
+            costLimit = CsvHelper.ConvertToInt(data.GetValue(rowIndex, "cost_limit"));
+            mitigTotal = CsvHelper.ConvertToInt(data.GetValue(rowIndex, "mitig_total"));
+            notes = data.GetValue(rowIndex, "notes");
+        }
+    }
+}
